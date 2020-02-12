cc0e45f4b2f62dcfb8c0d8ae6b810301a0e39ec1
Jens Uwe Becker <jens.becker@root-itsolutions.de>
2020-02-12 09:22:41 +0100

    Fixed mongoose reference

--- src/modules/examResult/examResult.schema.ts
+++ src/modules/examResult/examResult.schema.ts
@@ -10,14 +10,14 @@ export const ExamResultSchema = new mongoose.Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
     exam: {type: Schema.Types.ObjectId, ref: 'Exam'},
     martialArt: {
-        _id: {type: Schema.Types.ObjectId, required: true},
+        _id: {type: Schema.Types.ObjectId, ref: 'MartialArt'},
         name: {type: String, required: true},
         styleName: {type: String, required: true}
     },
     examiner: {
-        _id: {type: Schema.Types.ObjectId, required: true},
+        _id: {type: Schema.Types.ObjectId, ref: 'User'},
         firstName: {type: String, required: true},
-        lastName: {type: String, required: true},
+        lastName: {type: String, required: true}
     },
     rank: {type: String, required: true},
     date: {type: Date, required: true},
