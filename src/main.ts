876bf9e275ad3626475af0c06cdf35c990497e67
Jens Uwe Becker <jens.becker@root-itsolutions.de>
2020-02-18 10:50:28 +0100

    Preparing for google cloud deployment
    Made some nessesary changes for google cloud app engine deployment

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
-import { Config } from 'Config';
+import * as Express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
+/**
+ * Code for google cloud deployment
+ */
+const server = Express();
+
+// Routes for google to check server health
+server.get('/', (req, res) => res.send('ok'));
+server.get('/_ah/health', (req, res) => res.send('ok'));    
+
+
+/**
+ * Normal server startup
+ */
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter(server)); // ExpressAdapter required in order to work in google cloud
 
   app.useStaticAssets(join(__dirname, '/uploads'));
   app.setBaseViewsDir(join(__dirname, '/views'));
   app.setViewEngine('hbs');
   app.enableCors();
-  await app.listen(Config.PORT || 3000);
+  await app.listen( process.env.PORT || 3000);
 }
+
+
+
 bootstrap();
