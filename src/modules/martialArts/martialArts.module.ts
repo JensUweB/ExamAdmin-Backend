1196219d35e21b4ad8097b15df6ed6f1b4dca170
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-04-03 15:33:46 +0200

    added userModule import

--- src/modules/martialArts/martialArts.module.ts
+++ src/modules/martialArts/martialArts.module.ts
@@ -4,11 +4,13 @@ import { MartialArtSchema } from "./martialArts.schema";
 import { MartialArtsService } from "./martialArts.Service";
 import { MartialArtsResolver } from "./martialArts.resolver";
 import { AuthModule } from "../auth/auth.module";
+import { UserModule } from "../user/user.module";
 
 @Module({
     imports: [
         MongooseModule.forFeature([{name: 'MartialArt', schema: MartialArtSchema}]),
-        forwardRef(() => AuthModule)
+        forwardRef(() => AuthModule),
+        forwardRef(() => UserModule)
     ],
     providers: [MartialArtsResolver, MartialArtsService],
     exports: [MartialArtsService]
