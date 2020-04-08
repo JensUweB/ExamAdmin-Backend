6ae57abafce4ca09512d4e282b2e5381fb59c5d6
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-04-08 09:07:30 +0200

    removed import - file does no longer exist

--- src/modules/user/dto/maRanks.dto.ts
+++ src/modules/user/dto/maRanks.dto.ts
@@ -1,6 +1,5 @@
 import { ObjectType, Field, ID } from 'type-graphql';
 import { MartialArtsDto } from '../../martialArts/dto/martialArts.dto';
-import { RankPopDto } from './rank.pop.dto';
 
 @ObjectType()
 export class MaRanksDto {
