c2c53e38a2b4cb1b6fd0cc4c7a30a1348fff392e
JensUweB <58864964+JensUweB@users.noreply.github.com>
2020-04-09 14:30:38 +0200

    updated variables

--- environment.example.ts
+++ environment.example.ts
@@ -1,11 +1,15 @@
 export class environment {
     static PORT=3000;
-    static URL="http://localhost:3000";
-    static SMTP_HOST="smtp.localhost.com";
-    static SMTP_PORT=587;
-    static SMTP_SSL=false
-    static SERVER_EMAIL="noreply@localhost";
-    static EMAIL_PASS="123456";
+    static URL="http://api.localhost:3000";
+    static frontendUrl="http://app.localhost.net";
+    static SERVER_EMAIL="info@localhost.net";
+    static SMTP = {
+        HOST:"smtp.localhost.net",
+        PORT:587,
+        SSL:false,
+        USER:"myDefaultUser",
+        PASS:"SuperSecretPassword"
+    };
     static MONGO_CONN_STR="mongodb://localhost:27017/examadmin?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
     static JWT_SECRET="MIGrAgEAAiEAvwzN7rDBIe7D2kk3xIA6cKcDmLIc4uW3onudEG2be9ECAwEAAQIhALVc7Gl49f7GcLpZ40zxBYefuoRB1qC4PEH7ew2GGdOBAhEA/fu1sPvBYEE965VfIdKM2QIRAMCRKm2b8JnHCMt1298da7kCEBiY5wjD6P90chW9up/g70kCEFfOGXWPRF8qJTV40T/kD2kCEQCgk8BL1FMeyjD6Nbl7IJX7";
     static JWT_EXPIRE=7200;
